4828dfa68e6411962754ddd860f5fbd84f714299
Jonathan Ocampo <88061312+NoirPhantom@users.noreply.github.com>
2022-03-21 12:30:46 -0500

    Third Commit - adding extra examples

--- Collections/src/Calendar_Practice.java
+++ Collections/src/Calendar_Practice.java
@@ -13,5 +13,13 @@ public class Calendar_Practice {
         System.out.println("Current MINUTE: " + calendar.get(Calendar.MINUTE));
         System.out.println("Current SECOND: " + calendar.get(Calendar.SECOND));
 
+        // Returning the amount of days in a week
+        int max = calendar.getMaximum(Calendar.DAY_OF_WEEK);
+        System.out.println("Maximum number of days in a week: " + max);
+
+        // Returning the amount of weeks in a year
+        max = calendar.getMaximum(Calendar.WEEK_OF_YEAR);
+        System.out.println("Maximum number of weeks in a year: " + max);
+
     }
 }
--- Collections/src/Date_Practice.java
+++ Collections/src/Date_Practice.java
@@ -1,29 +1,36 @@
 import java.util.*;
 
 public class Date_Practice {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        Date d1 = new Date(2000, 11, 21);
-        Date d2 = new Date();
-        Date d3 = new Date(2010, 1, 3);
+                // Creating day with parameters
+                Date d1 = new Date(2000, 11, 21);
+                Date d2 = new Date();
+                Date d3 = new Date(2010, 1, 3);
 
-        boolean a = d3.after(d1);
-        System.out.println("Date d3 comes after " +
-                "date d2: " + a);
+                // Return true if d3 is after d1 or false
+                boolean a = d3.after(d1);
+                System.out.println("Date d3 comes after " +
+                                "date d2: " + a);
 
-        boolean b = d3.before(d2);
-        System.out.println("Date d3 comes before " +
-                "date d2: " + b);
+                // Returning true if d3 is before d2 or false
+                boolean b = d3.before(d2);
+                System.out.println("Date d3 comes before " +
+                                "date d2: " + b);
 
-        int c = d1.compareTo(d2);
-        System.out.println(c);
+                // Comparing if
+                int c = d1.compareTo(d2);
+                System.out.println(c);
 
-        System.out.println("Miliseconds from Jan 1 " +
-                "1970 to date d1 is " + d1.getTime());
+                // Returning the amount of milliseconds between the specific date to the date
+                // being specified
+                System.out.println("Miliseconds from Jan 1 " +
+                                "1970 to date d1 is " + d1.getTime());
 
-        System.out.println("Before setting: " + d2);
-        d2.setTime(204587433443L);
-        System.out.println("After setting: " + d2);
-    }
+                // Returning time and date before changing and after change
+                System.out.println("Before setting: " + d2);
+                d2.setTime(204587433443L);
+                System.out.println("After setting: " + d2);
+        }
 
 }
